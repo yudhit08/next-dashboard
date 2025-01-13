74e0f22efab70390d26b113ac5e5b5b3bfba0808
yudhit08 <yudhitrenandra99@gmail.com>
2025-01-13 22:53:44 +0700

    add: main functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import "@/app/ui/global.css"
 import { inter } from "@/app/ui/fonts";
 
+export const metadata = {
+    title: "Next Dashboard"
+}
+
 export default function RootLayout({
   children,
 }: {
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,4 +2,7 @@ import { Inter } from "next/font/google";
 import { Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Inter({subsets: ["latin"]})
+export const lusitana = Lusitana({
+	weight: ["400", "700"],
+	subsets: ["latin"],
+});
